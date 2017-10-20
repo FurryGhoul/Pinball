c4657ece95757076fb49996edf6865469a06b71e
Max <marc.garcia.u@gmail.com>
2017-10-20 19:25:16 +0200

    some problems solved

--- Physics2_class4 - handout/ModulePlayer.cpp
+++ Physics2_class4 - handout/ModulePlayer.cpp
@@ -1,5 +1,9 @@
 #include "Globals.h"
 #include "Application.h"
+#include "ModuleInput.h"
+#include "ModuleTextures.h"
+#include "ModuleSceneIntro.h"
+#include "ModulePhysics.h"
 #include "ModulePlayer.h"
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -27,8 +31,6 @@ bool ModulePlayer::CleanUp()
 // Update: draw background
 update_status ModulePlayer::Update()
 {
-<<<<<<< HEAD
-=======
 	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_DOWN)
 	{
 		App->scene_intro->leftflipper->body->ApplyAngularImpulse(-5.0f,true);
@@ -39,7 +41,6 @@ update_status ModulePlayer::Update()
 		App->scene_intro->rightflipper->body->ApplyAngularImpulse(5.0f, true);
 	}
 
->>>>>>> 5513f88c68895c0dd9d1c5a6de48789bd2238af1
 	return UPDATE_CONTINUE;
 }
 
